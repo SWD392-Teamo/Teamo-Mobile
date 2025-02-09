03a1f2ee091280d2a974ea778bfcc453133f192f
r1nzl3rrr <khanhle9344@gmail.com>
2025-02-09 19:11:51 +0700

    Khanh Le - Added custom theme, font family, color in tailwind #1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import "../styles/main.scss";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -14,7 +15,16 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    "Epilogue-Black": require("../assets/fonts/Epilogue/Epilogue-Black.ttf"),
+    "Epilogue-Bold": require("../assets/fonts/Epilogue/Epilogue-Bold.ttf"),
+    "Epilogue-ExtraBold": require("../assets/fonts/Epilogue/Epilogue-ExtraBold.ttf"),
+    "Epilogue-ExtraLight": require("../assets/fonts/Epilogue/Epilogue-ExtraLight.ttf"),
+    "Epilogue-Light": require("../assets/fonts/Epilogue/Epilogue-Light.ttf"),
+    "Epilogue-Medium": require("../assets/fonts/Epilogue/Epilogue-Medium.ttf"),
+    "Epilogue-Regular": require("../assets/fonts/Epilogue/Epilogue-Regular.ttf"),
+    "Epilogue-SemiBold": require("../assets/fonts/Epilogue/Epilogue-SemiBold.ttf"),
+    "Epilogue-Thin": require("../assets/fonts/Epilogue/Epilogue-Thin.ttf"),
+    "ClashDisplay": require('../assets/fonts/ClashDisplay.ttf'),
   });
 
   useEffect(() => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./styles/**/*.{js,jsx,ts,tsx,scss}",
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4CA4CD',
+        secondary: '#1C1C1C',
+        tertiary: '#FCFCFC',
+        grey: '#606261',
+        darkgrey: '#3E424A',
+      },
+      fontFamily: {
+        epthin: ["Epilogue-Thin", "sans-serif"],
+        epextralight: ["Epilogue-ExtraLight", "sans-serif"],
+        eplight: ["Epilogue-Light", "sans-serif"],
+        epregular: ["Epilogue-Regular", "sans-serif"],
+        epmedium: ["Epilogue-Medium", "sans-serif"],
+        epsemibold: ["Epilogue-SemiBold", "sans-serif"],
+        epbold: ["Epilogue-Bold", "sans-serif"],
+        epextrabold: ["Epilogue-ExtraBold", "sans-serif"],
+        epblack: ["Epilogue-Black", "sans-serif"],
+        clash: ["ClashDisplay", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
