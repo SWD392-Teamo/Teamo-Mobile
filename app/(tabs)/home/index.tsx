6686a052c4748834c488bb5a1b757826cea32a54
r1nzl3rrr <khanhle9344@gmail.com>
2025-02-23 15:21:11 +0700

    Khanh Le - Implemented message listener and handlers #11

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,3 +1,4 @@
+import LogoutButton from '@/components/LogoutButton'
 import React from 'react'
 import { Text, View } from 'react-native'
 
@@ -5,6 +6,10 @@ export default function HomeLanding() {
   return (
     <View className='flex justify-center items-center'>
       <Text className='text-primary text-xl font-bmedium'>Home</Text>
+
+      <View className='mt-10'>
+        <LogoutButton />
+      </View>
     </View>
   )
 }
