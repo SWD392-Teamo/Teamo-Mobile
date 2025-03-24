f701e48aecd5c1ddca0480f674b2bca99601a485
r1nzl3rrr <khanhle9344@gmail.com>
2025-03-24 16:07:00 +0700

    Khanh Le - fixed home page UI and group posts UI #48

--- app/(tabs)/home/GuideMajorCard.tsx
+++ app/(tabs)/home/GuideMajorCard.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 
 export default function GuideMajor() {
     const router = useRouter()
@@ -10,10 +10,15 @@ export default function GuideMajor() {
 
     return (
         <Pressable onPress={onGuideMajor}>
-            <View className="m-5">
+            <View className="m-3">
                 <View className="border-2 border-primary p-5 rounded-lg flex flex-col items-start justify-between text-center transition"
                       style={{ shadowOpacity: 0, elevation: 0 }}
                 >
+                    <Image
+                        className="w-full h-60" 
+                        source={{ uri: "https://firebasestorage.googleapis.com/v0/b/mechat-926e4.appspot.com/o/teamo%2Fimages%2Fplaceholders%2Fmajor-guide.png?alt=media" }}
+                        resizeMode="cover"
+                    />
                     <Text className="font-bbold text-primary text-bl">Choose Major</Text>
                     <Text className="mb-2 font-blight text-primary text-bm text-wrap">Start finding the perfect group</Text>
                 </View>
--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,15 +1,8 @@
+import PostsListing from '@/components/posts/PostsListing';
 import React from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import GuideMajorCard from './GuideMajorCard';
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View className = 'w-full flex justify-content-start'>  
-          <GuideMajorCard />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <PostsListing isNews={true} />
   )
 }
